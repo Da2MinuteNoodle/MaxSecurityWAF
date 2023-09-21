b5076328294105c3e4fec4c8ad8c76d5c833eb1d
Da2MinuteNoodle <maxmallan25@gmail.com>
2023-09-22 09:52:46 +1000

    Reverted middleware changes to be functioning.

--- WAFMiddleware.cs
+++ WAFMiddleware.cs
@@ -27,35 +27,14 @@ public class WAFMiddlewareService : IWAFMiddlewareService {
     }
 
 }
-
 public class WAFMiddleware {
     private IWAFMiddlewareService middlewareService;
 
     private RequestDelegate next;
 
-    private const string SqlInjectionPattern =
-        @"(\s*([\0\b\'\""""\n\r\t\%_\\]*\s*(((select\s*.+\s*from\s*.+)|(insert\s*.+\s*into\s*.+)|(update\s*.+\s*set\s*.+)|(delete\s*.+\s*from\s*.+)|(drop\s*.+)|(truncate\s*.+)|(alter\s*.+)|(exec\s*.+)|(\s*(all|any|not|and|between|in|like|or|some|contains|containsall|containskey)\s*.+[\=\>\<=\!\~]+.+)|(let\s+.+[\=]\s*.*)|(begin\s*.*\s*end)|(\s*[\/\*]+\s*.*\s*[\*\/]+)|(\s*(\-\-)\s*.*\s+)|(\s*(contains|containsall|containskey)\s+.*)))(\s*[\;]\s*)*)+)";
-
-    private Regex sqlInjectionRegex;
-
     public WAFMiddleware(IWAFMiddlewareService middlewareService, RequestDelegate next) {
         this.middlewareService = middlewareService;
-        this.next              = next;
-
-        sqlInjectionRegex = new(SqlInjectionPattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
-    }
-
-    public async Task<bool> SqlInjectionFilter(HttpResponse response) {
-        try {
-            var originalContent = await new StreamReader(response.Body).ReadToEndAsync();
-
-            if(originalContent is null)
-                return false;
-
-            return sqlInjectionRegex.IsMatch(originalContent);
-        } catch {
-            return false;
-        }
+        this.next = next;
     }
 
     public async Task InvokeAsync(HttpContext context) {
@@ -71,16 +50,11 @@ public class WAFMiddleware {
             .Select(r => r.Action)
             .Contains(WAFRuleAction.Allow);
 
-        if((!allow && !noAllowRules) || deny)
-            goto Reject;
-
-        if(await SqlInjectionFilter(context.Response)) {
+        if((!allow && !noAllowRules) || deny) {
             context.Response.StatusCode = 403;
+            return;
         }
 
         await next(context);
-
-    Reject:
-        context.Response.StatusCode = 403;
     }
 }
