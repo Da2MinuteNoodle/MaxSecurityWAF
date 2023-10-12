0ade01acc2e0e3997b46946f4e93199aef5d3d25
Laibakhd <113737646+Laibakhd@users.noreply.github.com>
2023-10-12 16:56:32 +1100

    Created Custom Authenticated State Provider Class

---
+++ CustomAuthenticationStateProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims; 
+
+namespace MaxSecurityWAF;
+
+	public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+       // throw new NotImplementedException();
+
+        var identity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, "admin"), }, "apiauth_type");
+
+        var user = new ClaimsPrincipal(identity);
+
+        return Task.FromResult(new AuthenticationState(user)); 
+    }
+
+    public void MarkUserAsAuthenticated(string username)
+    {
+        var identity = new ClaimsIdentity(new[]
+       {
+            new Claim(ClaimTypes.Name, username), }, "apiauth_type");
+
+        var user = new ClaimsPrincipal(identity);
+
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+}
+
